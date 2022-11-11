e1e91dbae649827d16f85f262dafff871e85bf49
Praveen S <praveensampath2706@gmail.com>
2022-11-11 11:52:28 +0530

    uploading to github

--- app.py
+++ app.py
@@ -1,9 +1,80 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import app
+import models
 
-@app.route("/")
+app = models.app
+db = models.db
+
+t_employee = models.Employee
+t_departments = models.Departments
+
+flag = True
+
+with app.app_context():
+    db.create_all()
+    
+def populate():
+  
+    emp_1 = t_employee(first_name='Praveen', last_name='S', email_address='iamprvn.s@gmail.com', phone='9988774455', salary='99,00,000')
+    db.session.add(emp_1)
+
+    emp_2 = t_employee(first_name='Sheldon', last_name='Cooper', email_address='sheldonor@gmail.com', phone='9988774455', salary='73,73,737')
+    db.session.add(emp_2)
+
+    emp_3 = t_employee(first_name='Leonard', last_name='Hofstadter', email_address='iamnotleakey@gmail.com', phone='9988774455', salary='72,00,000')
+    db.session.add(emp_3)
+
+    emp_4 = t_employee(first_name='	Howard', last_name='Wolowitz', email_address='austronautWolowitz@gmail.com', phone='9988774455', salary='69,42,000')
+    db.session.add(emp_4)
+
+    emp_5 = t_employee(first_name='Rajesh', last_name='Koothrappali', email_address='brown.dynamite@gmail.com', phone='9988774455', salary='1,00,00,000')
+    db.session.add(emp_5)
+
+    dep_1 = t_departments(dept_name='Production')
+    db.session.add(dep_1)
+
+    dep_2 = t_departments(dept_name='Developer')
+    db.session.add(dep_2)
+
+    dep_3 = t_departments(dept_name='Marketing')
+    db.session.add(dep_3)
+
+    dep_4 = t_departments(dept_name='R & D')
+    db.session.add(dep_4)
+
+    db.session.commit()
+
+@app.route("/",methods=['GET','POST'])
 def home():
-    return render_template("index.html")
+    populate()
+    return render_template('index.html')
+
+
+@app.route("/assignment",methods=['GET','POST'])
+def assignment():
+
+    if request.method == 'POST':
+        department_id = request.form['dept_select']
+        emp_id = request.form['emp_id']
+        employee = t_employee.query.filter_by(id=emp_id).first()
+        employee.dept_id = department_id
+
+        db.session.commit()
+
+        print(employee)
+        
+    emp_vals = t_employee.query.all()
+    dept_vals = t_departments.query.all()
+    
+    return render_template("table.html", employees=emp_vals, departments=t_departments, department_names=dept_vals)
+
+
+
+# @app.route("/test")
+# def test():
+#     results = t_employee.query.all()
+#     return str(results[0].first_name)
+
+
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,19 +1,62 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'culturemonkeyassignment'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
-db.init_app(app)
 
+db = SQLAlchemy(app)
 
-class Employee(db.Model):
+class Departments(db.Model):
+
+    __tablename__ = "departments"
+
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
+    dept_name = db.Column(db.String, nullable=True)
+
+    def __init__(self, dept_name) -> None:
+        super().__init__()
+        self.dept_name = dept_name
+
+
+    def __repr__(self) -> str:
+        return f"Departments('{self.id}','{self.dept_name}')"
+    
+class Employee(db.Model):
+
+    __tablename__ = "employee"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String)
-    email_address = db.Column(db.String)
+    email_address = db.Column(db.String,  nullable=False)
     phone = db.Column(db.String)
     salary = db.Column(db.String)
+    dept_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False, default=1)
+
+    def __init__(self, first_name, last_name, email_address, phone, salary, dept_id = 1) -> None:
+        super().__init__()
+        self.first_name = first_name 
+        self.last_name = last_name
+        self.email_address = email_address
+        self.phone = phone
+        self.salary = salary
+        self.dept_id = dept_id
+
+        
+
+    def __repr__(self) -> str:
+
+        res = {}
+        res[self.id] = {}
+        res[self.id]['first_name'] = self.first_name 
+        res[self.id]['last_name'] = self.last_name
+        res[self.id]['email_address'] = self.email_address
+        res[self.id]['phone'] = self.phone
+        res[self.id]['salary'] = self.salary
+        res[self.id]['dept_id'] = self.dept_id
+
+        return str(res)
 
